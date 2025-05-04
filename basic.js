f011dab0b0de6f821c14fbde444d269455ecd463
Rahat Ahmed Shafi <124520003+RahatAhmedShafi@users.noreply.github.com>
2025-05-04 12:28:44 +0600

    dataTypes

--- basic.js
+++ basic.js
@@ -1,9 +1,40 @@
-console.log("Welcome to Javascript");
-console.log("This is Commit"); // This is a comment
+// console.log("Welcome to Javascript");
+// console.log("This is Commit"); // This is a comment
 
-let accountEmail="jhon@gmail.com"
-let accountPassword="12345678"
-let accountCity="Dhaka"
+// let accountEmail="jhon@gmail.com"
+// let accountPassword="12345678"
+// let accountCity="Dhaka"
 
 
-console.table([accountEmail,accountPassword,accountCity])
+// console.table([accountEmail,accountPassword,accountCity])
+
+// let score="33"
+// console.log(typeof score)
+// let score1=Number(score)
+// console.log(typeof score1)
+
+// let number1=null
+// console.log(number1)
+// console.log(typeof number1) // null is an object in js
+
+// let number2=undefined
+// console.log(number2)
+// console.log(typeof number2) // undefined is a type in js
+
+// create a object
+// let user={
+//     name: "rahat",
+//     age:24,
+//     Proffession:"developer"
+// }
+// console.log(user)
+// console.table(user) // to show the object in a table format
+
+
+score=100
+const bonus=50
+score=150
+var score
+console.log(score+bonus) // 150
+
+
