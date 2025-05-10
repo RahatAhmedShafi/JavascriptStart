4b6b402ce436158673dd7b3ee29265cda70774d8
Rahat Ahmed Shafi <124520003+RahatAhmedShafi@users.noreply.github.com>
2025-05-10 15:07:10 +0600

    async and callback function

---
+++ advance.js
@@ -0,0 +1,25 @@
+// console.log('Hello from advance.js')
+// console.log('Hello from advance.js 2')
+// console.log('Hello from advance.js 3')
+
+        //-----Async function
+
+// console.log("program start");
+// console.log("Program exicution");
+
+// setTimeout(()=>{
+//     console.log("It's the time to break the code");
+// },4000)
+// console.log("Program end");
+
+                               //----callback function
+function add(a,b){
+   console.log("Adding two numbers: ",a+b)
+   
+}
+// function sub(a,b,sumcallback){
+//     sumcallback(a,b)
+// }
+// sub(5,6,add)
+
+setTimeout(add,3000,5,6)// // 5,6 are the arguments for add function and 3000 is the time
