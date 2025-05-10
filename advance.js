4c864603246ff546afedd0081169504beff425e2
Rahat Ahmed Shafi <124520003+RahatAhmedShafi@users.noreply.github.com>
2025-05-10 15:37:56 +0600

    callback hell

--- advance.js
+++ advance.js
@@ -13,13 +13,42 @@
 // console.log("Program end");
 
                                //----callback function
-function add(a,b){
-   console.log("Adding two numbers: ",a+b)
+// function add(a,b){
+//    console.log("Adding two numbers: ",a+b)
    
-}
-// function sub(a,b,sumcallback){
-//     sumcallback(a,b)
 // }
-// sub(5,6,add)
+// // function sub(a,b,sumcallback){
+// //     sumcallback(a,b)
+// // }
+// // sub(5,6,add)
 
-setTimeout(add,3000,5,6)// // 5,6 are the arguments for add function and 3000 is the time
+// setTimeout(add,3000,5,6)// // 5,6 are the arguments for add function and 3000 is the time
+
+// function getdata(getid){
+//    setTimeout(()=>{
+//     console.log(`Getting data from the server with id ${getid}`);
+    
+
+//    },2000)}
+
+// //    getdata(5)
+
+                               // callback hell
+function getdata(getid,callback){
+    setTimeout(() => {
+        console.log(`Getting data from the server with id ${getid}`);
+        if(callback){
+            callback();
+        }
+        
+        
+        
+    }, 2000);
+}
+
+getdata(5,()=>{
+    getdata(6,()=>{
+        getdata(7)
+    })
+})
+
